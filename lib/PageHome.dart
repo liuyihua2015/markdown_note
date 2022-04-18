7c67a6ac00422dbcaa8bfa1c154a29eb3c973d99
liuyihua <529732278@qq.com>
2022-04-18 21:04:26 +0800

    项目初始化

---
+++ lib/PageHome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PageHome extends StatefulWidget {
+  const PageHome({Key? key}) : super(key: key);
+
+  @override
+  State<PageHome> createState() => _PageHomeState();
+}
+
+class _PageHomeState extends State<PageHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text("markdown"),
+    );
+  }
+}
